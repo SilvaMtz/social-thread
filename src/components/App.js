d5ff02e63aa82637565a51fbc6942746db08161c
David Martinez de Silva <dmtzds@gmail.com>
2020-04-01 15:48:42 -0600

    connect react to blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('Non-Ethereum browser detected!');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    // Load account
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+  }
+
   render() {
     return (
       <div>
