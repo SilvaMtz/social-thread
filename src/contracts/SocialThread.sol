25898cd2ee19809efb94e7982d4eefa921ab0b1a
David Martinez de Silva <dmtzds@gmail.com>
2020-04-01 12:34:58 -0600

    Post struct and creation

--- src/contracts/SocialThread.sol
+++ src/contracts/SocialThread.sol
@@ -7,4 +7,22 @@ contract SocialThread {
   constructor() public {
     name = "The Social Thread";
   }
+
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address payable author;
+  }
+
+  mapping(uint => Post) public posts;
+  uint public postCount = 0;
+
+
+  function createPost(string memory _content) public {
+    // Increment the post count
+    postCount++;
+    posts[postCount] = Post(postCount, _content, 0, msg.sender);
+  }
+
 }
