d2b8fbf070e3c69a23d9a7c33699441bfcc8aa98
David Martinez de Silva <dmtzds@gmail.com>
2020-04-01 14:53:27 -0600

    test for tipping on post

--- test/SocialThread.test.js
+++ test/SocialThread.test.js
@@ -50,9 +50,41 @@ contract('SocialThread', ([deployer, author, tipper]) => {
     it('Lists posts', async() => {
       const post = await socialThread.posts(postCount);
       assert.equal(post.id.toNumber(), postCount.toNumber(), 'id is correct');
-      assert.equal(post.content, 'This is my first post', 'content is correct');
+      assert.equal(post.content, 'This is my first Post', 'content is correct');
       assert.equal(post.tipAmount, '0', 'tip amount is correct');
       assert.equal(post.author, author, 'author is correct');
+    });
+
+    it('allows users to tip posts', async() => {
+      // Track the author balance before purchase
+      let oldAuthorBalance;
+      oldAuthorBalance = await web3.eth.getBalance(author)
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance);
+
+      result = await socialThread.tipPost(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') });
+
+      // SUCCESS
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct');
+      assert.equal(event.content, 'This is my first Post', 'content is correct');
+      assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct');
+      assert.equal(event.author, author, 'author is correct');
+
+      // Check that author received funds
+      let newAuthorBalance;
+      newAuthorBalance = await web3.eth.getBalance(author);
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance);
+
+      let tipAmount;
+      tipAmount = web3.utils.toWei('1', 'Ether');
+      tipAmount = new web3.utils.BN(tipAmount);
+
+      const expectedBalance = oldAuthorBalance.add(tipAmount);
+
+      assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+      // FAILURE
+      await socialThread.tipPost(99, { from: tipper, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
     });
   });
 });
