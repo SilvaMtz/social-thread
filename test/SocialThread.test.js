7b76c000261e1a469af3349fe9027c5ed6bf34fb
David Martinez de Silva <dmtzds@gmail.com>
2020-04-01 13:08:56 -0600

    Post creation tests

--- test/SocialThread.test.js
+++ test/SocialThread.test.js
@@ -4,7 +4,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should();
 
-contract('SocialThread', (accounts) => {
+contract('SocialThread', ([deployer, author, tipper]) => {
   let socialThread;
 
   before(async() => {
@@ -12,18 +12,41 @@ contract('SocialThread', (accounts) => {
   });
 
   describe('deployment', async() => {
-    it('deploys successfully', async() => {
+    it('Deploys successfully', async() => {
       const address = await socialThread.address;
       assert.notEqual(address, 0x0);
       assert.notEqual(address, '');
       assert.notEqual(address, null);
       assert.notEqual(address, undefined);
-    })
+    });
 
-    it('has a name', async() => {
+    it('Has a name', async() => {
       const name = await socialThread.name();
       assert.equal(name, 'The Social Thread');
-    })
-  })
+    });
+  });
+
+  describe('posts', async() => {
+    let result, postCount;
+
+    before(async() => {
+      result = await socialThread.createPost('This is my first Post', { from: author });
+      postCount = await socialThread.postCount();
+    });
+
+    it('Creates posts', async() => {
+      // SUCCESS
+      assert.equal(postCount, 1);
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct');
+      assert.equal(event.content, 'This is my first Post', 'content is correct');
+      assert.equal(event.tipAmount, '0', 'tip amount is correct');
+      assert.equal(event.author, author, 'author is correct');
+
+      // FAILURE (Post has no content)
+      await socialThread.createPost('', { from: author }).should.be.rejected;
+    });
+
+  });
 
 });
