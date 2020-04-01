73bd372ed28d8da94c8ff6200d77a815be05e9be
David Martinez de Silva <dmtzds@gmail.com>
2020-04-01 12:45:24 -0600

    SocialThread contract postCreated event

--- src/contracts/SocialThread.sol
+++ src/contracts/SocialThread.sol
@@ -1,12 +1,10 @@
 pragma solidity ^0.5.0;
 
 contract SocialThread {
-  // State variable
-  string public name;
 
-  constructor() public {
-    name = "The Social Thread";
-  }
+  string public name;
+  mapping(uint => Post) public posts;
+  uint public postCount = 0;
 
   struct Post {
     uint id;
@@ -15,14 +13,29 @@ contract SocialThread {
     address payable author;
   }
 
-  mapping(uint => Post) public posts;
-  uint public postCount = 0;
+  event PostCreated(
+    uint id,
+    string content,
+    uint tipAmount,
+    address payable author
+  );
 
+  constructor() public {
+    name = "The Social Thread";
+  }
 
   function createPost(string memory _content) public {
+    // Raise and error and halt function if _content is blank
+    require(
+      bytes(_content).length > 0,
+      'Post content cannot be blank!'
+    );
     // Increment the post count
     postCount++;
+    // Create post by passing the post postCount as PostID, content, tip as 0, and the post's author
     posts[postCount] = Post(postCount, _content, 0, msg.sender);
+    // Trigger event
+    emit PostCreated(postCount, _content, 0, msg.sender);
   }
 
 }
